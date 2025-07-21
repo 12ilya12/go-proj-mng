d1f0e6328aa6e8c3433367df6aaacd44ff2c09db
Лопухин Илья <lopukhiniv@yandex.ru>
2025-07-22 01:48:23 +0300

    Исправлена ошибка при инициализации grpc-клиента для напоминаний. Скорректирован файл развёртывания проекта и readme.

--- initializers/initReminder.go
+++ initializers/initReminder.go
@@ -5,10 +5,11 @@ import (
 
 	reminder "github.com/12ilya12/go-proj-mng/reminder-service/gen/reminder"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func InitReminder() reminder.ReminderServiceClient {
-	conn, err := grpc.NewClient("reminder-service:50051")
+	conn, err := grpc.NewClient("reminder-service:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Нет соединения: %v", err)
 	}
